d5f21f67c613c0088ff5deb45b9e5ff45352274d
Keshav Kishor Ram <ramabishek40@gmail.com>
2021-08-26 23:13:03 +0545

    [2]Profile page partially completed

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 const userController = require("../controllers/user.controller");
 const userInfoController = require("../controllers/user.info.controller");
 const auth = require("../middlewares/auth");
-
+const User = require("../models/user.model");
+router.get("/all", async (req, res) => {
+  const allUsers = await User.find({}).select("-password");
+  res.send(allUsers);
+});
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
