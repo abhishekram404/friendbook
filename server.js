0d792b3fc7f72679e8e3d859f3a97792826f8304
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-02-18 16:23:34 +0545

    Build react app

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const userRoute = require("./routes/user.routes");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
+const path = require("path");
 dotenv.config();
 const app = express();
 app.use(cookieParser());
@@ -17,6 +17,18 @@ const isProduction = process.env.NODE_ENV === "production";
 require("./utils/dbConnection");
 
 app.use("/api/user", userRoute);
+
+if (isProduction) {
+  app.use(express.static(path.join(__dirname, "friendbook-frontend", "build")));
+  app.get("*", async (req, res) => {
+    return res
+      .status(200)
+      .sendFile(
+        path.join(__dirname, "friendbook-frontend", "build", "index.html")
+      );
+  });
+}
+
 // app.use("/api/", userRoute);
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
